e2a5c5f778c28ba01eee2dffa70912de2c5ed164
kerby <mail@kerby.ru>
2012-11-11 16:42:23 +0400

    db fix

--- cw_essentials/cwcore/install.sql
+++ cw_essentials/cwcore/install.sql
@@ -2,18 +2,22 @@ CREATE TABLE IF NOT EXISTS `prefix_commentwatcher_watcher_data` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `comment_id` int(11) unsigned DEFAULT NULL,
   `commented_id` int(11) unsigned DEFAULT NULL,
+  `ncomment_id` int(11) DEFAULT NULL,
+  `ncommented_id` int(11) DEFAULT NULL,
   `container_id` int(11) unsigned NOT NULL,
-  `container_type` enum('core') NOT NULL DEFAULT 'core',
+  `container_type` enum('lswall','talk','topic','core') NOT NULL DEFAULT 'core',
   `replier_id` int(11) unsigned NOT NULL,
   `owner_id` int(11) unsigned NOT NULL,
   `date_add` datetime NOT NULL,
   `comment_active` tinyint(1) NOT NULL DEFAULT '1',
-  `comment_type` enum('direct','later','indirect','favority') NOT NULL DEFAULT 'indirect',
+  `comment_type` enum('lswall_direct','lswall','newtalk','direct','later','indirect','favority') NOT NULL DEFAULT 'indirect',
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   KEY `o_ct_at` (`owner_id`,`container_type`,`comment_type`),
   KEY `prefix_commentwatcher_watcher_data_fk1` (`commented_id`),
-  KEY `fsearch` (`owner_id`,`container_type`,`comment_id`,`comment_type`,`comment_active`)
+  KEY `fsearch` (`owner_id`,`container_type`,`comment_id`,`comment_type`,`comment_active`),
+  KEY `prefix_commentwatcher_watcher_data_fk0` (`comment_id`),
+  KEY `prefix_commentwatcher_watcher_data_fk2` (`replier_id`)
 ) ENGINE=InnoDB;
 
 ALTER TABLE `prefix_commentwatcher_watcher_data`
