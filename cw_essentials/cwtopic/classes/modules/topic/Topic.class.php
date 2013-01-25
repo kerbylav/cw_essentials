a7fc5e30449d6ab7253c45d1b0285ae03fcd3675
kerby <mail@kerby.ru>
2013-01-25 06:31:07 +0400

    1.0.2 compatibility fix

--- cw_essentials/cwtopic/classes/modules/topic/Topic.class.php
+++ cw_essentials/cwtopic/classes/modules/topic/Topic.class.php
@@ -25,12 +25,12 @@ class PluginCWTopic_ModuleTopic extends PluginCWTopic_Inherit_ModuleTopic
 	 * @param  int  $iTopicId
 	 * @return bool
 	 */
-	public function DeleteTopicAdditionalData($iTopicId) {
+    public function DeleteTopicAdditionalData($iTopicId,$aPhotos=array()) {
 	    $aAnswer=$this->PluginCWCore_Watcher_GetDataItemsByContainerTypeAndContainerId('topic', $iTopicId);
 	    foreach ($aAnswer as $oAnswer)
 	        $oAnswer->delete();
 	    // Чистим кэш
 	    $this->Cache_Clean(Zend_Cache::CLEANING_MODE_MATCHING_TAG,array('PluginCWCore_ModuleWatcher_EntityData_delete'));
-	    return parent::DeleteTopicAdditionalData($iTopicId);
+	    return parent::DeleteTopicAdditionalData($iTopicId,$aPhotos);
 	}
 }
